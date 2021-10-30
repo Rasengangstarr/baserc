36e03029795bffa24584da99cafe27d6786c5050
Rasengangstarr <davidouthere@msn.com>
2021-10-30 20:48:09 +0100

    fixed a small issue

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ ln .bashrc $MYHOME/.bashrc
 rm $MYHOME/.vimrc
 ln .vimrc $MYHOME/.vimrc
 
-rm $MYHOME/.config/.starship.toml
-ln .starship.toml $MYHOME/.config/.starship.toml
+rm $MYHOME/.config/starship.toml
+ln starship.toml $MYHOME/.config/starship.toml
 
 rm $MYHOME/.alacritty.yml
 ln .alacritty.yml $MYHOME/.alacritty.yml 
